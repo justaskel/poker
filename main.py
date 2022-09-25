0763d8a9cd0e7753397ae759e4ce631c31a93c5f
Justas Keliuotis <justas@Justass-MacBook.lan>
2022-09-25 22:50:27 +0100

    asdf

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ class DeckOfCards:
     
     suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
     vals = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    strength = {
+        'J' : 11,
+        'Q' : 12,
+        'K' : 13,
+        'A' : 14
+    }
 
     def __init__(self):
         self.deck = [ y + " of " + x for x in self.suits for y in self.vals]
@@ -45,19 +51,78 @@ class Game(DeckOfCards):
     def start(self):
         list_cards = self.deal(3)
         str_cards = ', '.join(list_cards)
+        self.list_cards = list_cards
         print(f"Cards on the table: {str_cards}")
     
+    def calc_hand(self, hand):
+        lst = [x.split(" of")[0] for x in hand]
+        res = []
+        for x in lst:
+            try:
+                res.append(int(x))
+            except:
+                res.append(self.strength[x])
+        return res
     
+    def match_hand(self, hand):
+        lst_c = [x.split(" of")[0] for x in self.list_cards]
+        lst_h = [x.split(" of")[0] for x in hand]
+        res = []
+        for x in lst_c:
+            res.append(x) if x in lst_h else None
+        for x in lst_h:
+            res.append(x) if x in lst_c else None
+        return res
 
-if __name__ == "__main__":
+
+    def winner(self, hand_1, hand_2):
+        h_1 = self.match_hand(hand_1)
+        h_2 = self.match_hand(hand_2)
+        h_1 = self.calc_hand(h_1)
+        h_2 = self.calc_hand(h_2)
+
+        if sum(h_1) == 0 and sum(h_2) == 0:
+            mh_1 = max(self.calc_hand(hand_1))
+            mh_2 = max(self.calc_hand(hand_2))
+            if mh_1 > mh_2:
+                winner = 'p'
+            else:
+                winner = 'c'
+        elif sum(h_1) > sum(h_2):
+            winner = 'p'
+        else:
+            winner = 'c'
+        return winner
+
+    
+# print(f"Wellcome {p1_name}, how much money would you like to put into your bank?")
+# money = input()
+# p1.deposit(money)
+def play():
     print("Hi what's your name?")
     p1_name = input()
-    print(f"Wellcome {p1_name}, how much money would you like to put into your bank?")
-    money = input()
     p1 = Player()
-    p1.deposit(money)
+    p2 = Player()
     game = Game()
     player_hand = game.deal(2)
+    computer_hand = game.deal(2)
     p1.hand(player_hand)
+    p2.hand(computer_hand)
     game.start()
     print(f"{p1_name} hand: {', '.join(p1.hand)}")
+    print(f"Computer hand: {', '.join(p2.hand)}")
+    if game.winner(p1.hand, p2.hand) == 'p':
+        print(f"{p1_name} wins")
+    else:
+        print("Computer wins")
+
+if __name__ == "__main__":
+    end = False
+    play()
+    while not end:
+        print("Play again?")
+        answer = input()
+        if answer.lower().startswith('y'):
+            play()
+        else:
+            exit()
